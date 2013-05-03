745bcccd20f95a0b6bed0d49e202bc2fe7b90f80
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2013-05-03 19:02:43 +0400

    Top icons now (and top icons properties popup) now works via knockout; Some "file functions" (new, open, save, exit) moved from the separate .js file to the main viewmodel; Style refactoring.

--- scripts/file_functions.js
+++ scripts/file_functions.js
@@ -1,43 +1,6 @@
 var fileExtension = null;
 
 /* MENU COMMANDS */
-function newFileCommand() {
-	init();
-	
-	hideMainMenu();
-}
-
-function openFileCommand() {
-	var reader = new FileReader();
-	var image;
-    reader.onload = function(e) {
-        image = new Image();
-        image.src = e.target.result;
-		
-		image.onload = function() {
-			var canvas = getElement('canv');
-			canvas.width = image.width;
-			canvas.height = image.height;
-			canvas.style.width = canvas.width;
-			canvas.style.height = canvas.height;
-			
-			canvas.getContext("2d").drawImage(image, 0, 0);
-		};
-    };
-    reader.readAsDataURL(getElement('image_file_picker').files[0]);
-	
-	hideMainMenu();
-}
-
-function saveFileCommand() {
-	if (fileExtension == null)
-		saveAsPng();
-	else if (fileExtension == "png")
-		saveAsPng();
-	else if (fileExtension == "jpg")
-		saveAsJpeg();
-}
-
 function printCommand() {
 	hideMainMenu();
 }
@@ -56,10 +19,6 @@ function showPropertiesCommand() {
 
 function showAboutWindowCommand() {
 	hideMainMenu();
-}
-
-function exitCommand() {
-	window.close();
 }
 
 /* SAVE FUNCTIONS */
--- scripts/viewmodel.js
+++ scripts/viewmodel.js
@@ -1,36 +1,143 @@
-function PaintViewModel(){
-	var self = this;
-	
-	this.New = function(){
-	};
-	
-	this.Open = function(){
-	};
-	
-	this.Save = function(){
-	};
-	
-	this.SaveAs = function(){
-	};
-	
-	this.Print = function(){
-	};
-	
-	this.FromScanner = function(){
-	};
-	
-	this.SendEmail = function(){
-	};
-	
-	this.SetAsWallpaper = function(){
-	};
-	
-	this.ShowProperties = function(){
-	};
-	
-	this.ShowAbout = function(){
-	};
-	
-	this.Exit = function(){
-	};
+function TopIconViewModel(parent, name, description, offset, action) {
+    var self = this;
+
+    self.Visible = ko.observable(true);
+    self.Name = ko.observable(name);
+    self.Desctiption = ko.observable(description);
+    self.Offset = ko.observable(offset);
+
+    self.BackgroundStyle = ko.computed(function(){
+        return ' ' + self.Offset() + 'px 0';
+    });
+
+    this.ChangeState = function(){
+        self.Visible(!self.Visible());
+        parent.HideProperties();
+    };
+
+    this.Action = action;
+}
+
+function TopIconsViewModel(parent) {
+    var self = this;
+
+    self.PropertiesVisible = ko.observable(false);
+
+    self.Icons = ko.observableArray([]);
+
+    self.VisibleIcons = ko.computed(function(){
+        var result = [];
+
+        var i;
+        for (i = 0; i < self.Icons().length; i++)
+            if (self.Icons()[i].Visible() === true)
+                result.push(self.Icons()[i]);
+
+        return result;
+    });
+
+    this.ShowProperties = function () {
+        self.PropertiesVisible(true);
+    };
+
+    this.HideProperties = function () {
+        self.PropertiesVisible(false);
+    };
+
+    this.Init = function(){
+        self.Icons([
+            new TopIconViewModel(self, 'New', 'Create a new picture.', -16, parent.New),
+            new TopIconViewModel(self, 'Open', 'Open an existing picture.', -32, parent.Open),
+            new TopIconViewModel(self, 'Save', 'Save the current picture.', -48, parent.Save),
+            new TopIconViewModel(self, 'Print', 'Print the current picture.', -64, parent.Print),
+            new TopIconViewModel(self, 'Print preview', 'Print preview', -80, parent.Print),
+            new TopIconViewModel(self, 'Send in e-mail', 'Send a copy of the picture in an e-mail message as an attachment.', -100, parent.SendEmail),
+            new TopIconViewModel(self, 'Undo', 'Undo last action.', -118, parent.Undo),
+            new TopIconViewModel(self, 'Redo', 'Redo last action.', -136, parent.Redo)
+        ]);
+    };
+
+    self.Init();
+}
+
+function PaintViewModel() {
+    var self = this;
+
+    self.TopIcons = ko.observable();
+
+    self.UndoAvailable = ko.computed(function(){
+
+    });
+
+    self.RedoAvailable = ko.computed(function(){
+
+    });
+
+    this.New = function () {
+        init();
+
+        hideMainMenu();
+    };
+
+    this.Open = function () {
+        var reader = new FileReader();
+        var image;
+        reader.onload = function(e) {
+            image = new Image();
+            image.src = e.target.result;
+
+            image.onload = function() {
+                var canvas = getElement('canv');
+                canvas.width = image.width;
+                canvas.height = image.height;
+                canvas.style.width = canvas.width;
+                canvas.style.height = canvas.height;
+
+                canvas.getContext("2d").drawImage(image, 0, 0);
+            };
+        };
+        reader.readAsDataURL(getElement('image_file_picker').files[0]);
+
+        hideMainMenu();
+    };
+
+    this.Save = function () {
+        if (fileExtension == null)
+            saveAsPng();
+        else if (fileExtension == "png")
+            saveAsPng();
+        else if (fileExtension == "jpg")
+            saveAsJpeg();
+    };
+
+    this.SaveAs = function () {
+    };
+
+    this.Print = function () {
+    };
+
+    this.FromScanner = function () {
+    };
+
+    this.SendEmail = function () {
+    };
+
+    this.SetAsWallpaper = function () {
+    };
+
+    this.ShowProperties = function () {
+    };
+
+    this.ShowAbout = function () {
+    };
+
+    this.Exit = function () {
+        window.close();
+    };
+
+    this.Init = function(){
+        self.TopIcons(new TopIconsViewModel(self));
+    }
+
+    self.Init();
 }
