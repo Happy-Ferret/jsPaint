666f1832473b397381280a48e5784f058e1576c4
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2013-05-03 13:59:51 +0400

    Project: Added KO; Added viewmodel sketch for KO.

---
+++ scripts/viewmodel.js
@@ -0,0 +1,36 @@
+function PaintViewModel(){
+	var self = this;
+	
+	this.New = function(){
+	};
+	
+	this.Open = function(){
+	};
+	
+	this.Save = function(){
+	};
+	
+	this.SaveAs = function(){
+	};
+	
+	this.Print = function(){
+	};
+	
+	this.FromScanner = function(){
+	};
+	
+	this.SendEmail = function(){
+	};
+	
+	this.SetAsWallpaper = function(){
+	};
+	
+	this.ShowProperties = function(){
+	};
+	
+	this.ShowAbout = function(){
+	};
+	
+	this.Exit = function(){
+	};
+}
