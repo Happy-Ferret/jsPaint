b8ce3e837752a9cc3240d96f5136dab0b26b5d46
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2013-08-11 22:12:17 +0400

    Implemented buggy canvas resizing.

--- modules/canvas/canvasViewModel.js
+++ modules/canvas/canvasViewModel.js
@@ -1,3 +1,15 @@
+var ResizeStatus = {
+    None: 0,
+    North: 1,
+    West: 2,
+    South: 3,
+    East: 4,
+    NorthWest: 5,
+    NorthEast: 6,
+    SouthWest: 7,
+    SouthEast: 8
+};
+
 function CanvasViewModel(state) {
     var self = this;
 
@@ -63,109 +75,91 @@ function CanvasViewModel(state) {
         });
     };
 
+    self.ResizeMode = ResizeStatus.None;
     this.BeginResize = function () {
+        var oldCanvas = getElement('canv');
 
+        var newCanvas = document.createElement('canvas');
+        newCanvas.setAttribute("class", "resizing_canvas");
+        newCanvas.setAttribute("id", "resizing_canvas");
+
+        var position = getAbsoluteElementPosition(oldCanvas);
+        newCanvas.style.left = position.x;
+        newCanvas.style.top = position.y;
+        newCanvas.style.zIndex = "100";
+
+        newCanvas.height = oldCanvas.style.height;
+        newCanvas.width = oldCanvas.style.width;
+
+        getElement('canv').parentElement.appendChild(newCanvas);
     };
 
     this.StartResizeEast = function () {
-
+        self.ResizeMode = ResizeStatus.East;
+        self.BeginResize();
     };
 
     this.StartResizeSE = function () {
-
+        self.ResizeMode = ResizeStatus.SouthEast;
+        self.BeginResize();
     };
 
     this.StartResizeSouth = function () {
+        self.ResizeMode = ResizeStatus.South;
+        self.BeginResize();
+    };
 
+    this.ProcessResize = function (d, event) {
+        if (self.ResizeMode == ResizeStatus.None)
+            return;
+
+        var app = document.body;
+        var coords = getRelativeMousePos(app, event);
+
+        var resizingCanvas = getElement('resizing_canvas');
+
+        var x = coords.x - parseInt(resizingCanvas.style.left);
+        var y = coords.y - parseInt(resizingCanvas.style.top);
+
+        //updateCanvasSize({ width: x, height: y });
+
+        if (self.ResizeMode == ResizeStatus.East) {
+            resizingCanvas.width = x;
+            resizingCanvas.height = getElement('canv').height;
+        } else if (self.ResizeMode == ResizeStatus.South) {
+            resizingCanvas.height = y;
+            resizingCanvas.width = getElement('canv').width;
+        } else if (self.ResizeMode == ResizeStatus.SouthEast) {
+            resizingCanvas.width = x;
+            resizingCanvas.height = y;
+        }
     };
 
     this.EndResize = function () {
+        var canvas = getElement('canv');
+        var resizingCanvas = getElement('resizing_canvas');
+        var app = document.body;
 
+        canvas.style.width = resizingCanvas.width;
+        canvas.style.height = resizingCanvas.height;
+
+        canvas.width = parseInt(canvas.style.width) / 1;//getZoomValue();
+        canvas.height = parseInt(canvas.style.height) / 1;//getZoomValue();
+
+        getElement('workzone').style.height = canvas.height + "px";
+
+        canvas.parentElement.removeChild(resizingCanvas);
+
+        self.ResizeMode = ResizeStatus.None;
     };
 
     this.RenderCompleted = function () {
         self.InitGridlines();
     };
 
+    this.Test = function (a, b, c, d) {
+
+    };
+
     self.Init();
-}
-
-function initResizing() {
-    var oldCanvas = getElement('canv');
-
-    var newCanvas = document.createElement('canvas');
-    newCanvas.setAttribute("class", "resizing_canvas");
-    newCanvas.setAttribute("id", "resizing_canvas");
-
-    var position = getAbsoluteElementPosition(oldCanvas);
-    newCanvas.style.left = position.x;
-    newCanvas.style.top = position.y;
-    newCanvas.style.zIndex = "100";
-
-    newCanvas.height = oldCanvas.style.height;
-    newCanvas.width = oldCanvas.style.width;
-
-    getElement('application').appendChild(newCanvas);
-}
-
-function startResizingEast() {
-    initResizing();
-
-    resizingStatus = 1;
-}
-
-function startResizingSouth() {
-    initResizing();
-
-    resizingStatus = 2;
-}
-
-function startResizingSe() {
-    initResizing();
-
-    resizingStatus = 3;
-}
-
-function processResizing(event) {
-    if (resizingStatus == 0)
-        return;
-
-    var app = getElement('app');
-    var coords = getRelativeMousePos(app, event);
-
-    var resizingCanvas = getElement('resizing_canvas');
-
-    var x = coords.x - parseInt(resizingCanvas.style.left);
-    var y = coords.y - parseInt(resizingCanvas.style.top);
-
-    updateCanvasSize({ width: x, height: y });
-
-    if (resizingStatus == 1) {
-        resizingCanvas.width = x;
-        resizingCanvas.height = getElement('canv').height;
-    } else if (resizingStatus == 2) {
-        resizingCanvas.height = y;
-        resizingCanvas.width = getElement('canv').width;
-    } else if (resizingStatus == 3) {
-        resizingCanvas.width = x;
-        resizingCanvas.height = y;
-    }
-}
-
-function endResizing() {
-    var canvas = getElement('canv');
-    var resizingCanvas = getElement('resizing_canvas');
-    var app = getElement('app');
-
-    canvas.style.width = resizingCanvas.width;
-    canvas.style.height = resizingCanvas.height;
-
-    canvas.width = parseInt(canvas.style.width) / getZoomValue();
-    canvas.height = parseInt(canvas.style.height) / getZoomValue();
-
-    getElement('workzone').style.height = canvas.height + "px";
-
-    app.removeChild(resizingCanvas);
-
-    resizingStatus = 0;
 }
